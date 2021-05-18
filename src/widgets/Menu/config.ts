fd9ee6c7f1aa2bf68991ca4197271573c97af018
Kenny Yew <yoongyy@gmail.com>
2021-05-18 16:20:01 +0800

    Update Telegram and Twitter URL.

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -99,15 +99,11 @@ export const links = [
       // },
       {
         label: "Github",
-        href: "https://github.com/goosedefi/",
+        href: "https://github.com/greendefi/",
       },
       {
         label: "Docs",
-        href: "https://goosedefi.gitbook.io/goose-finance/",
-      },
-      {
-        label: "Blog",
-        href: "https://goosefinance.medium.com/",
+        href: "https://greendefi.gitbook.io/green-defi/",
       },
     ],
   },
@@ -120,67 +116,14 @@ export const socials = [
     items: [
       {
         label: "English",
-        href: "https://t.me/goosedefi",
-      },
-      // {
-      //   label: "Bahasa Indonesia",
-      //   href: "https://t.me/PancakeSwapIndonesia",
-      // },
-      {
-        label: "中文",
-        href: "https://t.me/goosefinancechinese",
-      },
-      // {
-      //   label: "Tiếng Việt",
-      //   href: "https://t.me/PancakeSwapVN",
-      // },
-      {
-        label: "Italiano",
-        href: "https://t.me/goosefinanceitalian",
-      },
-      {
-        label: "русский",
-        href: "https://t.me/goosefinancerussian",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/GooseFinanceTurkey",
-      },
-      // {
-      //   label: "Português",
-      //   href: "https://t.me/PancakeSwapPortuguese",
-      // },
-      {
-        label: "Español",
-        href: "https://t.me/goosefinancespanish",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/goosefinancejapanese",
-      },
-      {
-        label: "Français",
-        href: "https://t.me/goosefinancefrench",
-      },
-      {
-        label: "Announcements",
-        href: "https://t.me/goosefinanceann",
-      },
-      {
-        label: "Price Bot",
-        href: "https://t.me/BinanceRocketEGG",
+        href: "https://t.me/green_defi",
       },
     ],
   },
   {
     label: "Twitter",
     icon: "TwitterIcon",
-    href: "https://twitter.com/goosedefi",
-  },
-  {
-    label: "Reddit",
-    icon: "RedditIcon",
-    href: "https://www.reddit.com/r/GooseFinanceofficial",
+    href: "https://twitter.com/green_defi",
   },
 ];
 
