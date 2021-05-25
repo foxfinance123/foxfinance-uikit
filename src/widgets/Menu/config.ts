ead33757506c255c0522e7c5e4fb27e8e104f569
Kenny Yew <yoongyy@gmail.com>
2021-05-25 16:21:03 +0800

    Change Green Defi Twitter URL.

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -123,7 +123,7 @@ export const socials = [
   {
     label: "Twitter",
     icon: "TwitterIcon",
-    href: "https://twitter.com/defi_green",
+    href: "https://twitter.com/green_defi",
   },
 ];
 
